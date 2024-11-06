ef4d6ad7e4b5b13eb268fb04ba657706e8d57ec3
Victor-Gaudencio <victorrafaelg789@gmail.com>
2024-11-06 15:28:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("teste")
+const email = "test@test.com"
+const senha = "12345678"
+localStorage.setItem("email", "senha")
+
+const emailinput = document.getElementById("email")
+const senhainput = document.getElementById("senha")
+const formulario = document.getElementById("formulario_login")
+
+
+
+
+formulario.addEventListener("submit", (event) =>{    
+    event.preventDefault()
+    const emai1digitado = emailinput.value
+    const senhadigitado = senhainput.value
+    console.log(emailinput.value)
+    console.log(senhainput.value)
+
+})
+
+
+
+
+
+function login() {
+    const email = emailInput.value
+    const senha = senhaInput.value
+    if (localStorage.getItem(email) === senha){}
+
+}
