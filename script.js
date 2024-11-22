f2cf0d8b2458e865db35e8a6a6352c1ab31df5ff
Victor-Gaudencio <victorrafaelg789@gmail.com>
2024-11-22 17:21:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
-const emailinputLogin = document.getElementById("email")
-const senhainputLogin = document.getElementById("senha")
+const emailInputLogin = document.getElementById("email")
+const senhaInputLogin = document.getElementById("senha")
 const formularioLogin = document.getElementById("formulario_login")
-
 
 formularioLogin.addEventListener("submit", (event) => {
     event.preventDefault()
-    const emaildigitado = emailinputLogin.value
-    const senhadigitado = senhainputLogin.value
+    const emailDigitado = emailInputLogin.value
+    const senhaDigitada = senhaInputLogin.value
 
+    // Buscar usuarios no local storage
     const usuarios = JSON.parse(localStorage.getItem("usuarios"))
-    const usuariosencontrado = usuarios.find((usuario) => {
+    const usuarioEncontrado = usuarios.find((usuario) => {
         return (
-            usuario.email == emaildigitado &&
-            usuario.senha == senhadigitado
-
+            usuario.email == emailDigitado &&
+            usuario.senha == senhaDigitada
         )
-
     })
 
-    if (usuariosencontrado) {
+    if (usuarioEncontrado) {
         Swal.fire({
-            title: "tu tem o molhoo",
-            text: "entra fiaoo",
+            title: "Logado com sucesso!",
+            text: "Aproveite o sistema",
             timer: 1500,
             showConfirmButton: false,
             icon: "success"
-
         });
     } else {
         Swal.fire({
-            title: "Raposo nao pegue",
-            text: "RAPOSO NAO PEGUE",
-            timer: 1500,
-            showConfirmButton: false,
+            title: "Email ou Senha incorretos!",
+            text: "Tente novamente",
             icon: "error"
-        })
+        });
     }
 })
-
-
-
-
-
-
-function login() {
-    const email = emailInput.value
-    const senha = senhaInput.value
-    if (localStorage.getItem(email) === senha) { }
-
-}
-
-
--- script_usuario.js
+++ script_usuario.js
@@ -1,6 +1,11 @@
 const usuarios = JSON.parse(localStorage.getItem("usuarios"))
 const tabela = document.getElementById("tabela-usuarios")
 
+if (!usuarios) {
+    localStorage.setItem("usuarios", JSON.stringify([]))
+    location.reload()
+}
+
 for (let index = 0; index < usuarios.length; index++) {
     const usuario = usuarios[index];
 
@@ -9,9 +14,36 @@ for (let index = 0; index < usuarios.length; index++) {
             <td>${usuario.id}</td>
             <td>${usuario.email}</td>
             <td>${usuario.senha}</td>
+            <td>
+                <div class="btn btn-sm btn-warning">Editar</div>
+                <div class="btn btn-sm btn-danger" onclick="apagarUsuario(${usuario.id})">Excluir</div>
+            </td>
         </tr>
     `
-
     tabela.innerHTML += linha
-
 }
+
+function apagarUsuario(id) {
+    const usuarioEncontrado = usuarios.find((usuario) => {
+        return usuario.id == id
+    })
+    usuarios.splice(usuarios.indexOf(usuarioEncontrado), 1)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+    location.reload()
+}
+
+const formularioCadastro = document.getElementById("formulario_modal")
+formularioCadastro.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    const ultimoUsuario = usuarios[usuarios.length - 1]
+    const usuarioNovo = {
+        id: (ultimoUsuario?.id || 0) + 1,
+        email: document.getElementById("email_usuario").value,
+        senha: document.getElementById("senha_usuario").value
+    }
+
+    usuarios.push(usuarioNovo)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+    location.reload()
+})
+
